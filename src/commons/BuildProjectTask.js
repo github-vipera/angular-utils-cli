5d65b5729b59ccfe992b4007677a2dd597196ebb
Marco Bonati <marco.bonati@vipera.com>
2018-11-28 18:19:10 +0100

    Added build projects

---
+++ src/commons/BuildProjectTask.js
@@ -0,0 +1,24 @@
+var CommandExecutor = require('./CommandExecutor')
+const chalk = require('chalk');
+
+
+function BuildProjectTask(projectName, projectRoot, prod){
+    this.projectName = projectName;
+    this.projectRoot = projectRoot;
+    this.prod = prod;
+
+    let cmd = "ng build";
+    if (this.prod){
+        cmd = cmd + " --prod";
+    } 
+    this.runner = (new CommandExecutor).create(cmd, this.projectRoot, " * Building " + this.projectName);
+
+}
+
+BuildProjectTask.prototype.getRunner = function() {
+    //console.log("Starting build for " + chalk.green(this.projectName));
+    return this.runner;
+}
+
+module.exports = BuildProjectTask;
+
---
+++ src/commons/CommandExecutor.js
@@ -0,0 +1,41 @@
+const execShPromise = require("exec-sh").promise;
+const chalk = require('chalk');
+
+function CommandExecutor(){
+}
+
+/**
+ * Examples:
+ * ngutils build --prod 
+ */
+CommandExecutor.prototype.create = function(cmd, cwd, logTag) {
+
+    this.cmd = cmd;
+    this.cwd = cwd;
+    this.logTag = logTag;
+
+    this.run = async () => {
+        let out;
+
+        if (this.logTag){
+          console.log(chalk.green(this.logTag));
+        } else {
+          console.log(chalk.gray('Executing: ', this.cmd));
+        }
+        
+        try {
+          out = await execShPromise(cmd, { cwd: this.cwd });
+        } catch (e) {
+          console.log('Error: ', e);
+          //console.log('Stderr: ', e.stderr);
+          //console.log('Stdout: ', e.stdout);
+          return e;
+        }
+        //console.log('out: ', out.stdout, out.stderr);
+      }
+    return this.run;
+
+}
+
+// export the class
+module.exports = CommandExecutor;
